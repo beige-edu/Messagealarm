306c37e27764119aebb3e0b38a0851fecf450164
wuxiaochen <wuxc.eng@gmail.com>
2021-12-13 22:50:48 +0800

    fix: 修复linux编译问题

--- scripts/run_build.sh
+++ scripts/run_build.sh
@@ -13,7 +13,7 @@ fi
 # prepare dir
 mkdir -p ./bin
 # build demo app
-CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -o ./bin/PrometheusAlert main.go
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o ./bin/PrometheusAlert main.go
 
 
 # build and push docker image
