5334c6b9ca1a8ba6e00482d983399a0007913a6d
wuxiaochen <wuxc.eng@gmail.com>
2021-12-13 10:22:02 +0800

    feat: 变更打包地址

--- cmd/build.sh
+++
@@ -1,7 +0,0 @@
-set GO11MODULE=on
-set GO111MODULE=on
-set GOPROXY=https://goproxy.io
-# go mod init PrometheusAlert
-cd ..
-go mod vendor
-go build
--- scripts/run_build.sh
+++ scripts/run_build.sh
@@ -2,10 +2,11 @@
 
 _registry="$1"
 _tag="$2"
+_img="$3"
 _platform="linux/amd64,linux/arm64,linux/386"
 
-if [ -z "$_registry" ] || [ -z "$_tag" ]; then
-  echo "Please specify image repository and tag."
+if [ -z "$_registry" ] || [ -z "$_tag" ] || [ -z "$_img" ]; then
+  echo "Please specify image repository and tag img and "
   exit 0;
 fi
 
@@ -18,12 +19,11 @@ fi
 # prepare dir
 mkdir -p ./bin
 # build demo app
-app="messagealarm";
 CGO_ENABLED=0 go build -o ./bin/PrometheusAlert main.go
 # docker image
 docker buildx build --platform "$_platform" \
   -f "build/Dockerfile" \
-  -t "$_registry/$app:latest" \
+  -t "$_registry/$_img:latest" \
   --push .
 
 # clean dir bin
