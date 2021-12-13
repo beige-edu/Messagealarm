8e38e4727ccd918d2a96ca70400edac7ab8fbffd
wuxiaochen <wuxc.eng@gmail.com>
2021-12-13 17:59:25 +0800

    fix: 修改编译过程

--- scripts/run_build.sh
+++ scripts/run_build.sh
@@ -3,7 +3,7 @@
 _registry="$1"
 _tag="$2"
 _image="$3"
-_platform="linux/amd64,linux/arm64,linux/386"
+_platform="linux/arm64"
 
 if [ -z "$_registry" ] || [ -z "$_tag" ] || [ -z "$_image" ]; then
   echo "Please specify image repository and tag img and "
@@ -13,7 +13,12 @@ fi
 # prepare dir
 mkdir -p ./bin
 # build demo app
-CGO_ENABLED=0 GOOS=linux GOARCH= go build -o ./bin/PrometheusAlert main.go
+CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -o ./bin/PrometheusAlert main.go
+
+
+# build and push docker image
+docker build -f "build/Dockerfile" \
+  -t "$_repository/$_image:latest"
 
 docker push "$_repository/$_image:latest"
 
